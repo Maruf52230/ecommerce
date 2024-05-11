79a0008447dd0c6b84925165ccea1098d1dedd5c
Sayed Maruf <sayeedmuhammadabulalaalmaruf@gmail.com>
2024-05-11 21:09:13 +0600

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class WebScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String link = 'https://www.startech.com.bd/';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: Uri.parse(link)),
+        initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            javaScriptEnabled: true,
+            transparentBackground: true,
+          ),
+        ),
+        onProgressChanged: (controller, progress) {
+          // Update loading bar.
+        },
+        onLoadStart: (controller, url) {
+          // Handle page started loading.
+        },
+        onLoadStop: (controller, url) {
+          // Handle page finished loading.
+        },
+        onLoadError: (controller, url, code, message) {
+          print("Load error: $code, $message");
+        },
+      ),
+    );
+  }
+}
